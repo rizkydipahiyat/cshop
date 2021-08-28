2dbaa1e5bd496ebb59d3bc4266ee358eba3ffa7d
rizkydipahiyat <60695993+rizkydipahiyat@users.noreply.github.com>
2021-08-28 18:53:21 +0700

    User Register Reducer, Action & Screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -50,6 +50,9 @@ const Header = () => {
 								</LinkContainer>
 								{userInfo ? (
 									<NavDropdown title={userInfo.name} id="username">
+										<LinkContainer to="/profile">
+											<NavDropdown.Item>Profile</NavDropdown.Item>
+										</LinkContainer>
 										<NavDropdown.Item onClick={logoutHandler}>
 											Logout
 										</NavDropdown.Item>
