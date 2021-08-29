f88bbf58b7e769cb4bb5db8496f21f29b874669a
rizkydipahiyat <60695993+rizkydipahiyat@users.noreply.github.com>
2021-08-29 16:44:01 +0700

    Order Details Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,13 +12,17 @@ import {
 	userRegisterReducer,
 	userUpdateProfileReducer,
 } from "./reducers/userReducers";
-import { orderCreateReducer } from "./reducers/orderReducers";
+import {
+	orderCreateReducer,
+	orderDetailsReducer,
+} from "./reducers/orderReducers";
 
 const reducer = combineReducers({
 	productList: productListReducer,
 	productDetails: productDetailsReducer,
 	cart: cartReducer,
 	orderCreate: orderCreateReducer,
+	orderDetails: orderDetailsReducer,
 	userLogin: userLoginReducer,
 	userRegister: userRegisterReducer,
 	userDetails: userDetailsReducer,
